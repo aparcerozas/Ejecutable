e7b0fa9d9be8595b2556ff68e1ba546c33216a2f
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2019-01-15 09:49:59 +0100

    InicioLicencia

---
+++ Ejecutable/src/ejecutable/Ejecutable.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ */
+package ejecutable;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aparcerozas
+ */
+public class Ejecutable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+    }
+    
+}
